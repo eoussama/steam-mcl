c698bbc3be0bbf75c762e65a800d3ecfa21a5710
EOussama <oussama_gtacj@live.fr>
2019-09-14 17:02:45 +0100

    Clean up

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -8,24 +8,6 @@ import { SearchService } from 'src/app/services/search.service';
 })
 export class SearchComponent implements OnInit {
 
-  cards = [
-    {
-      icon: 'games',
-      title: 'Games',
-      description: 'Lookup prequesl, sequels, spin-offs and all games that are related to what you already own.'
-    },
-    {
-      icon: 'widgets',
-      title: 'DLCs',
-      description: 'Lookup missing DLCs for the games you already own.'
-    },
-    {
-      icon: 'apps',
-      title: 'Packages',
-      description: 'Lookup packages that includes games you already own.'
-    }
-  ];
-
   constructor(private search: SearchService) { }
 
   ngOnInit(): void { }
