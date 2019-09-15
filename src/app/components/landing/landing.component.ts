57367b773b130348525328a82930b4ddc215e8fc
EOussama <oussama_gtacj@live.fr>
2019-09-15 14:27:03 +0100

    Minor changes

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -12,17 +12,22 @@ export class LandingComponent {
     {
       icon: 'games',
       title: 'Games',
-      description: 'Lookup prequels, sequels, spin-offs and all games that are related to what you already own.'
+      description: 'Lookup prequels, sequels, spin-offs, and everything related to the games you already own.\
+      Putting together a collection has never been easier.'
     },
     {
       icon: 'widgets',
       title: 'DLCs',
-      description: 'Lookup missing DLCs for the games you already own.'
+      description: 'Lookup missing DLCs for the games you already own by the press of a button. \
+      Line up together every piece of extra content of the games you own. Svae up the pain of manually looking \
+      them up, and invest more time and energy in enjoying them instead.'
     },
     {
       icon: 'apps',
       title: 'Packages',
-      description: 'Lookup packages that includes games you already own.'
+      description: 'Lookup packages that your games are part of for easier related content consumption.\
+       You never want to bother with manually looking up the games that make your favorite franchise. \
+       Have them all reported to you with no more hassle.'
     }
   ];
 }
