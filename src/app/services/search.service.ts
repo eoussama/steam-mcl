35811a462849f71960bb19f43e8d8e259948e107
EOussama <oussama_gtacj@live.fr>
2019-09-15 12:47:21 +0100

    Added more validation to the input

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -20,101 +20,105 @@ export class SearchService {
    */
   async getSteamID(searchTerm: string): Promise<ISteamIDResult> {
 
-    // Checking of the input is numeric
-    if (Validator.isNumeric(searchTerm)) {
+    // Checking of the input is available
+    if (searchTerm.length > 0) {
 
-      // Getting the Steam ID validity
-      const validity = await this.isValidID(searchTerm);
+      // Checking of the input is numeric
+      if (Validator.isNumeric(searchTerm)) {
 
-      // Checking if the Steam ID is valid
-      if (validity.response.players.length > 0) {
-
-        return {
-          id: searchTerm,
-          input: searchTerm,
-          type: SteamIDTypes.ID64
-        };
-      } else {
-        throw new Error();
-      }
-    } else {
-
-      // Declaring the URL expression
-      const regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-
-      // Checking if the input is a valid steam URL
-      if (searchTerm.match(regex) !== null) {
-
-        // Parsing the URL
-        const url: URL = new URL(searchTerm);
-
-        // Check if the URL is pointing to Steam
-        if (url.hostname.includes('steamcommunity')) {
-
-          // Getting the route
-          const route: string[] = url
-            .pathname
-            .split('/')
-            .filter((str: string) => str.length > 0);
-
-          // Checking if URL is pointing at id
-          if (route[0] === 'id') {
-
-            // Getting the Steam results
-            const res: any = await this.getSteamIDFromName(route[1]);
-
-            // Getting the Steam ID
-            const steamid: string = res.response.steamid;
-
-            // Checking if the Steam ID is valid
-            if (steamid) {
-
-              return {
-                id: steamid,
-                input: searchTerm,
-                type: SteamIDTypes.ProfileURL
-              };
-            } else {
-              throw new Error();
-            }
-
-            // Checking if URL is pointing at profiles
-          } else if (route[0] === 'profiles') {
-
-            // Getting the Steam ID validity
-            const validity = await this.isValidID(route[1]);
-
-            // Checking if the Steam ID is valid
-            if (validity.response.players.length > 0) {
-
-              return {
-                id: route[1],
-                input: searchTerm,
-                type: SteamIDTypes.ProfilePermalink
-              };
-            } else {
-              throw new Error();
-            }
-          }
-        }
-      } else {
-
-        // Getting the Steam results
-        const res: any = await this.getSteamIDFromName(searchTerm);
-
-        // Getting the Steam ID
-        const steamid: string = res.response.steamid;
+        // Getting the Steam ID validity
+        const validity = await this.isValidID(searchTerm);
 
         // Checking if the Steam ID is valid
-        if (steamid) {
+        if (validity.response.players.length > 0) {
 
           return {
-            id: steamid,
+            id: searchTerm,
             input: searchTerm,
-            type: SteamIDTypes.Nickname
+            type: SteamIDTypes.ID64
           };
         } else {
           throw new Error();
+        }
+      } else {
+
+        // Declaring the URL expression
+        const regex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+
+        // Checking if the input is a valid steam URL
+        if (searchTerm.match(regex) !== null) {
+
+          // Parsing the URL
+          const url: URL = new URL(searchTerm);
+
+          // Check if the URL is pointing to Steam
+          if (url.hostname.includes('steamcommunity')) {
+
+            // Getting the route
+            const route: string[] = url
+              .pathname
+              .split('/')
+              .filter((str: string) => str.length > 0);
+
+            // Checking if URL is pointing at id
+            if (route[0] === 'id') {
+
+              // Getting the Steam results
+              const res: any = await this.getSteamIDFromName(route[1]);
+
+              // Getting the Steam ID
+              const steamid: string = res.response.steamid;
+
+              // Checking if the Steam ID is valid
+              if (steamid) {
+
+                return {
+                  id: steamid,
+                  input: searchTerm,
+                  type: SteamIDTypes.ProfileURL
+                };
+              } else {
+                throw new Error();
+              }
+
+              // Checking if URL is pointing at profiles
+            } else if (route[0] === 'profiles') {
+
+              // Getting the Steam ID validity
+              const validity = await this.isValidID(route[1]);
+
+              // Checking if the Steam ID is valid
+              if (validity.response.players.length > 0) {
+
+                return {
+                  id: route[1],
+                  input: searchTerm,
+                  type: SteamIDTypes.ProfilePermalink
+                };
+              } else {
+                throw new Error();
+              }
+            }
+          }
+        } else {
+
+          // Getting the Steam results
+          const res: any = await this.getSteamIDFromName(searchTerm);
+
+          // Getting the Steam ID
+          const steamid: string = res.response.steamid;
+
+          // Checking if the Steam ID is valid
+          if (steamid) {
+
+            return {
+              id: steamid,
+              input: searchTerm,
+              type: SteamIDTypes.Nickname
+            };
+          } else {
+            throw new Error();
+          }
         }
       }
     }
