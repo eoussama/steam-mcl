45c5df9bd831a64d14260bfb6e0d6c6f3e1691c2
EOussama <oussama_gtacj@live.fr>
2019-09-21 20:40:51 +0100

    Added a loading event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'steam-mcl';
+
+  /**
+   * The loading event listener
+   *
+   * @param loading The loading state
+   */
+  loadingEventListener(loading: boolean): void {
+    console.log({ loading });
+  }
 }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { SearchService } from 'src/app/services/search.service';
 import { ISteamIDResult } from 'src/app/models/steamidresult';
 
@@ -7,11 +8,14 @@ import { ISteamIDResult } from 'src/app/models/steamidresult';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+
+  /**
+   * The loading state event emitter
+   */
+  @Output() loadingChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(private search: SearchService) { }
-
-  ngOnInit(): void { }
 
   /**
    * The event of the search input
@@ -19,6 +23,9 @@ export class SearchComponent implements OnInit {
    * @param e The event object
    */
   onSearchChanged(e: Event) {
+
+    // Emitting the loading event
+    this.loadingChanged.emit(true);
 
     // Getting the input
     const searchInput = e.target as HTMLInputElement;
@@ -33,6 +40,11 @@ export class SearchComponent implements OnInit {
       })
       .catch((err: string) => {
         console.error(err);
+      })
+      .finally(() => {
+
+        // Emitting the loading event
+        this.loadingChanged.emit(false);
       });
   }
 }
