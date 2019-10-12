70980279f513c4eef3f3cb2539a274a49b87e1f0
EOussama <oussama_gtacj@live.fr>
2019-10-12 10:43:03 +0100

    Added lookup card

--- src/app/components/lookup/lookup.component.ts
+++ src/app/components/lookup/lookup.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 import { SearchService } from 'src/app/services/search.service';
 
 import { ISearchResult } from 'src/app/models/searchresult';
+import { ESearchResultTypes } from 'src/app/enums/searchresulttypes.enum';
 
 @Component({
   selector: 'app-lookup',
   templateUrl: './lookup.component.html',
-  styleUrls: ['./lookup.component.css']
+  styleUrls: ['./lookup.component.scss']
 })
 export class LookupComponent implements OnInit {
+
+  //#region Properties
+
+  /**
+   * The state of the container
+   * - Failure
+   * - Success
+   * - Loading
+   */
+  state: ESearchResultTypes;
+
+  //#endregion
 
   //#region Constructor
 
@@ -20,7 +33,9 @@ export class LookupComponent implements OnInit {
    */
   constructor(
     private searchService: SearchService
-  ) { }
+  ) {
+    this.state = ESearchResultTypes.Loading;
+  }
 
   //#endregion
 
@@ -34,11 +49,13 @@ export class LookupComponent implements OnInit {
     // Checking if the search result object is valid
     if (searchResult) {
       console.log({ searchResult });
+      this.state = searchResult.state;
     }
 
     // Subscribing to the search event
     this.searchService.searchEvent.subscribe((searchResult: ISearchResult) => {
       console.log({ searchResult });
+      // this.state = searchResult.state;
     });
   }
 
--- src/app/components/lookup/lookup.module.ts
+++ src/app/components/lookup/lookup.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MatCardModule } from '@angular/material/card';
+
 import { LookupComponent } from './lookup.component';
 
 const routes: Routes = [
@@ -12,6 +14,7 @@ const routes: Routes = [
   declarations: [LookupComponent],
   imports: [
     CommonModule,
+    MatCardModule,
     RouterModule.forChild(routes)
   ]
 })
