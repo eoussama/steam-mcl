6cbb695a4e3242509b20b6aff577cebf14193125
EOussama <oussama_gtacj@live.fr>
2019-11-05 20:48:25 +0100

    Added user data model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -6,6 +6,41 @@ import { IApp } from './app';
 export interface IUser {
 
   /**
+   * The Steam ID of the user
+   */
+  id: string;
+
+  /**
+   * The name of the user
+   */
+  username: string;
+
+  /**
+   * The real name of the user
+   */
+  realname: string;
+
+  /**
+   * The profile's url
+   */
+  profileurl: string;
+
+  /**
+   * The avatar's link
+   */
+  avatar: string;
+
+  /**
+   * Creation timestamp
+   */
+  timecreated: number;
+
+  /**
+   * Last log-off timestamp
+   */
+  lastlogoff: number;
+
+  /**
    * The apps list
    */
   apps: IApp[];
