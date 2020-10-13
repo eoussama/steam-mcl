83d4fbd98009ec3eaf19942878b8a7ac528c7419
Oussama Essamadi <oussama.essamadi@inovasquad.com>
2020-10-13 10:22:09 +0100

    Disabled third party cors on production, fixes #7.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiKey: '368F4B94FC9034A9A0261EA60AF74BB0',
-  apiEndpoint: '//api.steampowered.com/',
-  cors: 'https://cors-anywhere.herokuapp.com/http:'
+  apiEndpoint: 'http://api.steampowered.com/',
+  cors: ''
 };
