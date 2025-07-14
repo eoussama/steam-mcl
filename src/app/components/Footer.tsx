149d25d80fa0a4c5687b2100135d0e1ff868743a
eoussama <oussama_gtacj@live.fr>
2025-07-14 11:23:41 +0100

    refact: update import path in Footer component for consistency

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "../lib/helpers";
+import { cn } from "@/lib/helpers";
 import { ExternalLink } from "./ExternalLink";
 import packageJson from "../../../package.json";
 
