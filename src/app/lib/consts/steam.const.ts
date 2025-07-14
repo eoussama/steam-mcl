8aaf95c3109277738b2676642660e1a0583de104
eoussama <oussama_gtacj@live.fr>
2025-07-14 10:44:25 +0100

    refact: remove steam API service module and consolidate constants in steam.const.ts

--- src/app/lib/consts/steam.const.ts
+++ src/app/lib/consts/steam.const.ts
@@ -1,1 +1,9 @@
 export const STEAM_API_BASE_URL = "https://api.steampowered.com";
+
+export const STEAM_API_ENDPOINTS = {
+  GET_APP_LIST: "/ISteamApps/GetAppList/v2/",
+  GET_OWNED_GAMES: "/IPlayerService/GetOwnedGames/v1/",
+  RESOLVE_VANITY_URL: "/ISteamUser/ResolveVanityURL/v1/",
+  GET_PLAYER_SUMMARIES: "/ISteamUser/GetPlayerSummaries/v2/",
+  GET_USER_STATS: "/ISteamUserStats/GetUserStatsForGame/v2/",
+} as const;
--- src/app/lib/helpers/index.ts
+++ src/app/lib/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from "./cn.helper";
 export * from "./theme.helper";
 export * from "./query.helper";
+export * from "./steam.helper";
